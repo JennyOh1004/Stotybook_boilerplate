8ef55ed67da09252e4abcfe025d8fce84c6977b2
JennyOh1004 <ohseulki1004@gmail.com>
2020-06-23 17:14:08 -0700

    storybook boilerplate

---
+++ src/components/Jenny.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class Jenny extends Component {
+  render() {
+    return <div>HI</div>;
+  }
+}
+
+export default Jenny;
